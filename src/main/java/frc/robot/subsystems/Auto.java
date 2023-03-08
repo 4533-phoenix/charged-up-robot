a066365a952c82a38924e5a12497ded0da3a781c
BreadStixFTW <77415101+BreadStixFTW@users.noreply.github.com>
2023-03-08 16:42:23 -0500

    Added autonomous that does nothing

--- src/main/java/frc/robot/subsystems/Auto.java
+++ src/main/java/frc/robot/subsystems/Auto.java
@@ -25,6 +25,7 @@ public final class Auto extends Subsystem {
 
     private static final Map<String, Action> autoCommands = Map.ofEntries(
         Map.entry("Test Autonomous", AutoActions.testAutonomous()),
+        Map.entry("Do Nothing", AutoActions.doNothing()),
         Map.entry("Blue Bottom Cube Autonomous", AutoActions.blueBottomCubeAutonomous())
     );
 
@@ -72,6 +73,10 @@ public final class Auto extends Subsystem {
     }
 
     private static final class AutoActions {
+        public static final Action doNothing() {
+            return new Action(() -> {}, () -> {}, () -> {}, ActionConstants.WILL_CANCEL);
+        }
+
         public static final Action testAutonomous() {
             Pose2d startPose = new Pose2d(new Translation2d(), PoseEstimator.getInstance().getSwerveRotation());
   
