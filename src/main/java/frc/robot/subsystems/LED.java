4399c26cc7d0d05e178c22f62b93380029d288d4
drakeerv <drakeerv@outlook.com>
2023-04-01 12:44:16 -0400

    Update LED Code

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems;
 
 import frc.robot.Constants.*;
+
+import java.util.stream.IntStream;
+
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.Subsystem;
@@ -10,6 +13,23 @@ public final class LED implements Subsystem {
     public AddressableLEDBuffer ledBuffer;
 
     public LED() {}
+
+    public enum Colors {
+        BLUE(153, 153, 2),
+        YELLOW(66, 247, 245),
+        PURPLE(140, 32, 137),
+        BLANK(0, 0, 0);
+    
+        public final int r;
+        public final int g;
+        public final int b;
+    
+        Colors(int r, int g, int b) {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+    }
 
     public enum LEDState {
         YELLOW_AND_BLUE, PURPLE, YELLOW, OFF
@@ -27,26 +47,35 @@ public final class LED implements Subsystem {
         ledStrip.start();
     }
 
+    public void fillAllLEDs(Colors color) {
+        IntStream.range(1, this.ledBuffer.getLength())
+            .forEach(i -> this.ledBuffer.setRGB(i, color.r, color.g, color.b));
+    }
+
+    public void fillIntervalLEDs(int interval, Colors[] colors) {
+        IntStream.iterate(0, i -> i + interval)
+            .limit((ledBuffer.getLength() + interval - 1) / interval)
+            .forEach(i -> {
+                int colorIndex = i % colors.length;
+                ledBuffer.setRGB(i, colors[colorIndex].r, colors[colorIndex].g, colors[colorIndex].b);
+            });
+    }
+
     public void setLEDState(LEDState state) {
         if (state.equals(LEDState.YELLOW_AND_BLUE)) {
-            for (int i = 1; i < this.ledBuffer.getLength(); i += 2) {
-                this.ledBuffer.setRGB(i - 1, 66, 247, 245);
-                this.ledBuffer.setRGB(i, 153, 153, 2);
-            }
+            Colors[] colors = new Colors[] {
+                Colors.YELLOW,
+                Colors.BLUE
+            };
+            fillIntervalLEDs(2, colors);
         } else if (state.equals(LEDState.PURPLE)) {
-            for (int i = 1; i < this.ledBuffer.getLength(); i++) {
-                this.ledBuffer.setRGB(i, 140, 32, 137);
-            }
+            fillAllLEDs(Colors.PURPLE);
         } else if (state.equals(LEDState.YELLOW)) {
-            for (int i = 1; i < this.ledBuffer.getLength(); i++) {
-                this.ledBuffer.setRGB(i, 153, 153, 2);
-            }
+            fillAllLEDs(Colors.YELLOW);
         } else if (state.equals(LEDState.OFF)) {
-            for (int i = 1; i < this.ledBuffer.getLength(); i++) {
-                this.ledBuffer.setRGB(i, 0, 0, 0);
-            }
+            fillAllLEDs(Colors.BLANK);
         }
-
+    
         this.ledStrip.setData(this.ledBuffer);
     }
 
